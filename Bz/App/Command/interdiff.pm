584b06f051c3545390e8bce20038fd83ada8a84e
Byron Jones <bjones@mozilla.com>
2014-04-02 15:17:42 +0800

    "interdiff" should support downloaded patches

--- Bz/App/Command/interdiff.pm
+++ Bz/App/Command/interdiff.pm
@@ -18,6 +18,16 @@ sub execute {
             $a_i <=> $b_i;
         }
         glob($workdir->dir . '_*.patch');
+    if (!@files) {
+        @files =
+            sort {
+                my ($a_i) = $a =~ /^\d+-(\d+)/;
+                my ($b_i) = $b =~ /^\d+-(\d+)/;
+                $a_i <=> $b_i;
+            }
+            grep { /^\d+-\d+\.patch$/ }
+            glob($workdir->dir . '*.patch');
+    }
     splice(@files, 0, -2);
     die "failed to find 2 patches\n" unless scalar(@files) == 2;
     info("interdiff'ing @files");
