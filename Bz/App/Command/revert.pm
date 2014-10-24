a5d29f7710b886926d4f2e71c334f95cb9003fdf
Byron Jones <bjones@mozilla.com>
2014-10-24 13:25:37 +0800

    change revert to leave untracked files by default

--- Bz/App/Command/revert.pm
+++ Bz/App/Command/revert.pm
@@ -5,13 +5,30 @@ use Bz;
 use File::Path qw(remove_tree);
 
 sub abstract {
-    return "reverts all changes made to an instance";
+    return "reverts changes made to an instance.";
+}
+
+sub usage_desc {
+    return "bz revert [--all]";
+}
+
+sub opt_spec {
+    return (
+        [ "all|a",  "revert all changes (removes untracked files)" ],
+    );
+}
+
+sub description {
+    return <<EOF;
+be default all changes made to tracked files will be reverted.
+passing in --all or -a will additionally delete untracked files.
+EOF
 }
 
 sub execute {
     my ($self, $opt, $args) = @_;
 
-    info("reverting all local changes and commits");
+    info("reverting" . ($opt->all ? " all" : "") . " local changes and commits");
     my $current = Bz->current;
     $current->unfix() if $current->is_workdir;
     $current->git(
@@ -19,7 +36,11 @@ sub execute {
         'origin/' . $current->branch,
         '--hard',
     );
-    $current->git(qw(clean -f -d));
+    if ($opt->all) {
+        $current->git(qw(clean --force -d));
+    } else {
+        $current->git(qw(status --short));
+    }
     $current->git('pull');
     $current->fix() if $current->is_workdir;
 }
