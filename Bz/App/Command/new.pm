873f2fe17fb18893c9c89d7f36bc069f69c3d60d
Byron Jones <bjones@mozilla.com>
2014-10-31 14:13:03 +0800

    detect the repo/db from the dir name

--- Bz/App/Command/new.pm
+++ Bz/App/Command/new.pm
@@ -44,6 +44,14 @@ sub execute {
         info(sprintf("Bug %s: %s", $bug->id, $bug->summary));
     }
 
+    # creating a dir with the name 123456-bmo should default to 'bmo'
+    # likewise for -trunk
+    if (!@$args && $dir =~ /-bmo$/) {
+        push @$args, 'bmo';
+    } elsif (!@$args && $dir =~ /-trunk$/) {
+        push @$args, 'trunk';
+    }
+
     # use bmo defaults if just 'bmo' is provided, likewise for 'trunk'
     my ($repo_name, $db);
     if (scalar(@$args) == 1 && $args->[0] eq 'bmo') {
