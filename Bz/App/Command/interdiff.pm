2ee966ad1b42646a7f735378b45a8654c75757cc
Byron Jones <bjones@mozilla.com>
2015-05-18 12:28:18 +0800

    add poor-man's interdiff fallback

--- Bz/App/Command/interdiff.pm
+++ Bz/App/Command/interdiff.pm
@@ -2,6 +2,8 @@ package Bz::App::Command::interdiff;
 use parent 'Bz::App::Base';
 use Bz;
 
+use IPC::System::Simple qw(runx capturex EXIT_ANY);
+
 sub abstract {
     return "executes interdiff on the two most recently generated patches.";
 }
@@ -48,7 +50,66 @@ sub execute {
 
     die "failed to find 2 patches\n" unless scalar(@files) == 2;
     info("interdiff'ing @files");
-    system "interdiff @files";
+    return unless runx(EXIT_ANY, 'interdiff', @files);
+
+    # interdiff failed, just diff the patches
+    warning("interdiff failed, using standard diff");
+
+    my @lines = capturex(EXIT_ANY, 'diff', '-u', @files);
+    chomp(@lines);
+
+    # read diff into file/hunks
+    my @interdiff;
+    my ($current_file, $current_hunk);
+    foreach my $line (@lines) {
+        if ($line =~ /^--- ([^\t]+)/) {
+            $current_file = { name => $1, hunks => [], preamble => [ $line ] };
+            undef $current_hunk;
+            push @interdiff, $current_file;
+
+        } elsif ($line =~ /^\@\@ /) {
+            $current_hunk = [ $line ];
+            push @{ $current_file->{hunks} }, $current_hunk;
+
+        } elsif (!$current_hunk) {
+            push @{ $current_file->{preamble} }, $line;
+
+        } else {
+            push @$current_hunk, $line;
+        }
+    }
+
+    # remove files/hunks that only contain unimportant changes
+    foreach my $file (@interdiff) {
+        foreach my $hunk (@{ $file->{hunks} }) {
+            my $has_content = 0;
+            foreach my $line (@$hunk) {
+                next unless $line =~ /^[\-\+]/;
+                next if $line =~ /^-index / || $line =~ /^\+index /;
+                next if $line =~ /-\@\@ / || $line =~ /^\+\@\@ /;
+                $has_content = 1;
+                last;
+            }
+            if (!$has_content) {
+                $hunk = undef;
+            }
+        }
+        $file->{hunks} = [ grep { defined } @{ $file->{hunks} } ];
+    }
+    @interdiff = grep { scalar(@{ $_->{hunks} }) } @interdiff;
+
+    # display
+    foreach my $file (@interdiff) {
+        foreach my $line (@{ $file->{preamble} }) {
+            print $line, "\n";
+        }
+        foreach my $hunk (@{ $file->{hunks} }) {
+            foreach my $line (@$hunk) {
+                $line =~ s/^(.)./$1/;
+                print $line, "\n";
+            }
+        }
+    }
 }
 
 1;
