f6d4bac3d03aba8f57d105cb4a2b553e0bb2adff
Byron Jones <bjones@mozilla.com>
2015-07-28 23:48:02 +0800

    use api_key for auth

--- Bz/Bugzilla.pm
+++ Bz/Bugzilla.pm
@@ -3,8 +3,10 @@ use Bz;
 use Moo;
 
 use Bz::Bugzilla;
-use HTTP::Cookies;
-use XMLRPC::Lite;
+use JSON::XS qw(decode_json);
+use LWP::UserAgent;
+use URI;
+use URI::QueryParam;
 
 use constant BUG_FIELDS => [qw(
     id
@@ -17,29 +19,11 @@ use constant BUG_FIELDS => [qw(
     assigned_to
 )];
 
-has _proxy          => ( is => 'lazy' );
-has _cookiejar      => ( is => 'lazy' );
-has _authenticated  => ( is => 'rw', builder => 1);
-has _bug_cache      => ( is => 'rw', default => sub { {} } );
+has _ua        => ( is => 'lazy');
+has _bug_cache => ( is => 'rw', default => sub { {} } );
 
-sub _build__proxy {
-    my ($self) = @_;
-    return XMLRPC::Lite->proxy(
-        "https://bugzilla.mozilla.org/xmlrpc.cgi",
-        'cookie_jar' => $self->_cookiejar,
-    );
-}
-
-sub _build__cookiejar {
-    return HTTP::Cookies->new(
-        file        => Bz->config->user_path . '/cookie_jar.txt',
-        autosave    => 1,
-    );
-}
-
-sub _build__authenticated {
-    my ($self) = @_;
-    return $self->_is_authenticated();
+sub _build__ua {
+    return LWP::UserAgent->new( agent => 'bz-dev' );
 }
 
 sub bug {
@@ -47,10 +31,9 @@ sub bug {
     die "missing id" unless $bug_id;
 
     if (!exists $self->_bug_cache->{$bug_id}) {
-        my $response = $self->_rpc(
-            'Bug.get',
+        my $response = $self->_get(
+            'bug/' . $bug_id,
             {
-                ids => [ $bug_id ],
                 include_fields => BUG_FIELDS,
             }
         );
@@ -69,11 +52,11 @@ sub bugs {
     }
 
     if (@fetch_ids) {
-        my $response = $self->_rpc(
-            'Bug.get',
+        my $response = $self->_get(
+            'bug/',
             {
                 ids => \@fetch_ids,
-                include_fields => BUG_FIELDS,
+                include_fields => join(',', @{ BUG_FIELDS() }),
             }
         );
         foreach my $bug (@{ $response->{bugs} }) {
@@ -92,8 +75,8 @@ sub user {
     my ($self, $login) = @_;
     die "missing login" unless $login;
 
-    my $response = $self->_rpc(
-        'User.get',
+    my $response = $self->_get(
+        'user',
         {
             names => [ $login ],
             include_fields => [ 'name', 'real_name' ],
@@ -109,10 +92,9 @@ sub user {
 sub attachments {
     my ($self, $bug_id) = @_;
     die "missing bug_id" unless $bug_id;
-    return $self->_rpc(
-        'Bug.attachments',
+    return $self->_get(
+        "bug/$bug_id/attachment",
         {
-            ids => [ $bug_id ],
             exclude_fields => [ 'data' ],
         }
     )->{bugs}->{$bug_id} // [];
@@ -121,60 +103,39 @@ sub attachments {
 sub attachment {
     my ($self, $attach_id) = @_;
     die "missing attach_id" unless $attach_id;
-    my $attachments = $self->_rpc(
-        'Bug.attachments',
-        {
-            attachment_ids => [ $attach_id ],
-        }
+    my $attachments = $self->_get(
+        "bug/attachment/$attach_id"
     );
     return $attachments->{attachments}->{$attach_id}
         || die "failed to get attachment $attach_id information\n"
 
 }
 
-sub _rpc {
-    my ($self, @args) = @_;
+sub _get {
+    my ($self, $endpoint, $args) = @_;
+    die "config file missing bmo_api_key\n" unless Bz->config->bmo_api_key;
+    $args //= {};
 
-    if (!$self->_authenticated) {
-        my $login = Bz->config->bugzilla_mozilla_org_login;
-        info("password for $login on bugzilla.mozilla.org required:");
-        my $password = password("passsword:");
-        $self->_call(
-            'User.login',
-            {
-                login       => $login,
-                password    => $password,
-            }
-        );
-        die "authentication failed\n"
-            unless $self->_is_authenticated();
-        $self->_authenticated(1);
+    my $uri = URI->new('https://bugzilla.mozilla.org/rest/' . $endpoint);
+    foreach my $name (sort keys %$args) {
+        $uri->query_param($name => $args->{$name});
     }
+    print STDERR $uri->as_string, "\n";
 
-    return $self->_call(@args);
-}
+    my $request = HTTP::Request->new('GET', $uri->as_string);
+    $request->header( Content_Type => 'application/json' );
+    $request->header( Accept => 'application/json' );
+    $request->header( X_Bugzilla_API_Key => Bz->config->bmo_api_key );
 
-sub _call {
-    my ($self, @args) = @_;
-    my $response = $self->_proxy->call(@args);
-    if ($response->fault) {
-        die $response->faultstring . "\n";
+    my $response = $self->_ua->request($request);
+    if ($response->code !~ /^2/) {
+        my $error = $response->message;
+        eval {
+            $error = decode_json($response->decoded_content)->{message};
+        };
+        die $error . "\n";
     }
-    return $response->result;
-}
-
-sub _is_authenticated {
-    my ($self) = @_;
-    my $response = eval {
-        $self->_call(
-            'User.valid_login',
-            {
-                login   => Bz->config->bugzilla_mozilla_org_login,
-            }
-        );
-        1;
-    };
-    return $response ? 1 : 0;
+    return decode_json($response->decoded_content);
 }
 
 1;
